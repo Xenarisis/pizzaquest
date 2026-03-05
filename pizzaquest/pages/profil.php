0f86fc1c4917fbf3d9178c6fb06d7ce7145129e1
xen <romainrobert.xen@gmail.com>
2026-03-05 11:42:48 +0100

    feat: add orders table for all orders and add some function in db.php

--- pizzaquest/pages/profil.php
+++ pizzaquest/pages/profil.php
@@ -7,15 +7,34 @@ require_once ROOT . '/includes/header.php';
 // echo '</pre>';
 // exit;
 
-if (!isset($_SESSION['user_id'])):
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['is_connected']) || $_SESSION['is_connected'] !== true):
     header('Location: /pages/login.php');
     exit();
 endif;
 
 ?>
+<div class="container-fluid">
+    <h1 class="mt-5">Bienvenue sur votre profil</h1>
 
-<h1>Bienvenue sur votre profil</h1>
+    <div class="container-fluid align-items-center p-5">
+        <h3>Voici vos informations</h3>
+        <?php 
+        $pdo = getDB();
+        $stmt = $pdo->prepare('SELECT * FROM users WHERE id = :id');
+        $stmt->execute([':id' => $_SESSION['user_id']]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-<a class="nav-link" href="/pages/logout.php">Logout</a>
+        echo show_user_info($user);
+        ?>
+    </div>
+
+    <h1 class="mt-2">Si vous souhaitez modifier vos informations c'est juste ici:</h1>
+
+    
+
+    <div class="d-flex justify-content-center">
+        <a class="nav-link" href="/pages/logout.php">Logout</a>
+    </div>
+</div>
 
 <?php require_once ROOT . '/includes/footer.php'; ?>
